067c22db16e8c8562504c28ae900be0f7128fa0f
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-29 11:53:21 +0700

    summary all expenses

--- expense-tracker.py
+++ expense-tracker.py
@@ -32,6 +32,9 @@ def main():
 
     list_parser = subparsers.add_parser("list", help="View all expenses")
 
+    sum_parser = subparsers.add_parser("summary", help="A summary of all expenses")
+    sum_parser.add_argument("--month", type=int, help="for a specific month")
+
     args = parser.parse_args()
 
     if args.command == "add":
@@ -65,6 +68,33 @@ def main():
                 print(f"{str(exp["id"]):7} {exp["date"]:15} {exp["description"]:15} ${str(exp["amount"]):10}")
         else:
             print("The list is empty")
+    elif args.command == "summary":
+        months = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+        if args.month is None:
+            total_expenses = 0
+            for exp in data["expenses"]:
+                total_expenses += exp["amount"]
+            print(f"Total expenses: ${total_expenses}")
+        else:
+
+            total_expenses = 0
+            for exp in data["expenses"]:
+                if args.month == datetime.strptime(exp["date"], "%Y-%m-%d").month:
+                    total_expenses += exp["amount"]
+            print(f"Total expenses for {months[args.month]}: ${total_expenses}")
     else:
         parser.print_help()
 
